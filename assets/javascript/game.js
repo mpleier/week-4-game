dcf9d3d447b62c675ae8eb948296c2ef79975af7
mpleier <mpleier@gmail.com>
2017-07-29 21:08:01 -0700

    Works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,119 +1,65 @@
-//     console.log("working");
-var words = ["Awkward", "Bagpipes", "Banjo", "Bungler", "Croquet", "Crypt", "Dwarves", "Fervid", "Fishhook", "Fjord", "Gazebo", "Gypsy", "Haiku", "Haphazard", "Hyphen", "Ivory", "Jazzy", "Jiffy", "Jinx", "Jukebox", "Kayak", "Kiosk", "Klutz", "Memento", "Mystify", "Numbskull", "Ostracize", "Oxygen", "Pajama", "Phlegm", "Pixel", "Polka", "Quad", "Quip", "Rhythmic", "Rogue", "Sphinx", "Squawk", "Swivel", "Toady", "Twelfth", "Unzip", "Waxy", "Wildebeest", "Yacht", "Zealous", "Zigzag", "Zippy", "Zombie"];
+var targetNumber = 0;
+var win = 0;
+var loss = 0;
+var current = 0;
+var gem1 = 0;
+var gem2 = 0;
+var gem3 = 0;
+var gem4 = 0;
 
-var chars = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-    "u", "v", "w", "x", "y", "z"
-];
+$(document).ready(function() {
+  $("#gem1").on("click", function() {
+    click(gem1);
+  });
+  $("#gem2").on("click", function() {
+    click(gem2);
+  });
+  $("#gem3").on("click", function() {
+    click(gem3);
+  });
+  $("#gem4").on("click", function() {
+    click(gem4);
+  });
 
-var answer = randomWord();
-var tall = 10;
-var high = 1;
+function reset(){
 
-function box(a){
+  targetNumber = Math.floor(Math.random() * 50)+50;
+  current = 0;
+  gem1 = Math.floor(Math.random() * 10)+1;
+  gem2 = Math.floor(Math.random() * 10)+1;
+  gem3 = Math.floor(Math.random() * 10)+1;
+  gem4 = Math.floor(Math.random() * 10)+1;
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.clearRect(0, 0, 80, 300);
-ctx.fillRect(0,0,80,a);
+
+  update();
 }
 
-var last = "";
+reset();
 
-var latest = "";
+  function update(){
+  $("#target")
+    .html("Your target number is:<br><br>" + targetNumber);
+  $("#score")
+    .html("Wins: " + win + "<br><br>Losses: " + loss);
+  $("#total")
+    .html(current);
+  }
 
-function restart() {
-    last = answer;
-    answer = randomWord();
-    key.length = 0;
-    key = answer.split("")
-    show.length = 0;
-    show = answer.split("");
-    show.fill("_");
-    high = 1;
-    tall=10;
-    print();
-    box(tall);
-
-}
-
-function print() {
-  document.getElementById("body").innerHTML = ("<h1>Hangman</h1><br><br> Guess what word I\'m thinking of<br><br>Press a letter key on your keyboard to guess a letter.<br><br>The last answer was: " + last + "<br><br>Wins: " + wins + "<br><br>Losses: " + losses + "<br><br>Guesses Left: " + guesses + "<br><br>Your Guesses so far: " + a + "<br>" + show);
-  box(tall);
-}
-
-
-function randomWord() {
-    var char1 = words[Math.floor(Math.random() * 49)];
-    return "" + char1.toLowerCase();
-}
-
-function randomLetter() {
-    var char1 = chars[Math.floor(Math.random() * 26)];
-    return "" + char1;
-}
-
-var key = answer.split("")
-
-var show = answer.split("");
-show.fill("_");
-
-var letter = randomLetter();
-var wins = 0;
-var losses = 0;
-var guesses = 9;
-var a = [];
-var page;
-//
-// letter = randomLetter();
-
-// key.forEach(function(item, i) { if (item == guess) show[i] = guess; });
-
-
-
-// document.write("<h1>The Psychic Game</h1><br><br> Guess what letter I\'m thinking of<br><br>Wins: "+wins+"<br><br>Losses: "+losses+"<br><br>Guesses Left: "+guesses+"<br><br>Your Guesses so far: "+a)
-
-document.onkeydown = checkKey;
-
-function checkKey(e) {
-    var event = window.event ? window.event : e;
-    latest = event.key.toLowerCase();
-    key.forEach(function(item, i) {
-        if (item == latest) show[i] = latest;
-    });
-    if (a.indexOf(latest) < 0 && chars.indexOf(latest) > -1) {
-
-        a.push(latest);
-        guesses = guesses--;
-
-
-        if (key.toString(key) == show.toString(show)) {
-            wins++;
-            guesses = 9;
-            letter = randomLetter();
-            a = [];
-            z = restart();
-            z = print();
-
-        } else if (guesses <= 1) {
-            losses++;
-            guesses = 9;
-            letter = randomLetter();
-            a = [];
-            z = restart();
-            z = print();
-        } else {
-            if (key.indexOf(latest) > -1) {
-                z = print();
-            } else {
-                guesses--;
-		              high++;
-                  tall = high*10;
-                z = print();
-            }
-
-        }
+  function click(a){
+    current += a;
+    if (current > targetNumber){
+      loss++;
+      reset();
     }
-}
+    else if (current === targetNumber) {
+      win++;
+      reset();
+    }
+    update();
+  }
+
+
+
+
+
+});
