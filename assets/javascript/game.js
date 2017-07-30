c2d6446df855677a835d678c9e388a43e1374e9b
mpleier <mpleier@gmail.com>
2017-07-30 01:04:52 -0700

    Consolidated and genericized click and key presses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,44 +8,10 @@ var gem3 = 0;
 var gem4 = 0;
 
 $(document).ready(function() {
-  $("#gem1").on("click", function() {
-    click(gem1);
+
+$("[id^=gem]").on("click", function() {
+    click(eval($(this).attr("id")));
   });
-  $("#gem2").on("click", function() {
-    click(gem2);
-  });
-  $("#gem3").on("click", function() {
-    click(gem3);
-  });
-  $("#gem4").on("click", function() {
-    click(gem4);
-  });
-
-function reset(){
-
-  targetNumber = Math.floor(Math.random() * 50)+50;
-  current = 0;
-  gem1 = Math.floor(Math.random() * 10)+1;
-  gem2 = Math.floor(Math.random() * 20)+1;
-  gem3 = Math.floor(Math.random() * 30)+1;
-  gem4 = Math.floor(Math.random() * 40)+1;
-    if ((gem1 + gem2 + gem3 / target) % 1 === 0){
-      gem4 = 1;
-    }
-
-  update();
-}
-
-reset();
-
-  function update(){
-  $("#target")
-    .html("Your target number is:<br><br>" + targetNumber);
-  $("#score")
-    .html("Wins: " + win + "<br><br>Losses: " + loss);
-  $("#total")
-    .html(current);
-  }
 
   function click(a){
     current += a;
@@ -60,28 +26,56 @@ reset();
     update();
   }
 
+  // $("#gem1").on("click", function() {
+  //   click(gem1);
+  // });
+  // $("#gem2").on("click", function() {
+  //   click(gem2);
+  // });
+  // $("#gem3").on("click", function() {
+  //   click(gem3);
+  // });
+  // $("#gem4").on("click", function() {
+  //   click(gem4);
+  // });
+
+function reset(){
+
+  targetNumber = Math.floor(Math.random() * 50)+50;
+  current = 0;
+  gem1 = Math.floor(Math.random() * 10)+1;
+  gem2 = Math.floor(Math.random() * 20)+1;
+  gem3 = Math.floor(Math.random() * 30)+1;
+  gem4 = Math.floor(Math.random() * 40)+1;
+    if ((gem1 + gem2 + gem3 / targetNumber) % 1 === 0){
+      gem4 = 1;
+    }
+
+    update();
+  }
+
+  reset();
+
+  function update(){
+  $("#target")
+    .html("Your target number is:<br><br>" + targetNumber);
+  $("#score")
+    .html("Wins: " + win + "<br><br>Losses: " + loss);
+  $("#total")
+    .html(current);
+  }
+
+
+
 document.onkeydown = checkKey;
 
   function checkKey(e) {
-      var event = window.event ? window.event : e;
-      latest = event.key;
+   var event = window.event ? window.event : e;
+    latest = event.key;
+    if (latest % 1 === 0) {
+      click(eval("gem"+latest));
+    }
 
-
-switch(latest) {
-    case "1":
-        click(gem1);
-        break;
-    case "2":
-        click(gem2);
-        break;
-    case "3":
-        click(gem3);
-        break;
-    case "4":
-        click(gem4);
-        break;
-    default:
-
-}}
+}
 
 });
